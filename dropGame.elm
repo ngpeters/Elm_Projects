31c5eade3297278e6a23a7c7b1e542adbead741d
ngpeters <27ngpeters@gmail.com>
2017-05-03 16:54:10 -0600

    Clear on win

--- dropGame.elm
+++ dropGame.elm
@@ -23,23 +23,30 @@ main = program
 
 generateInitialSeed = Random.generate InitialSeed (Random.int Random.minInt Random.maxInt)
 
+
 type alias Model = { droplist : List Drop
                     , time : Int
                     , singleCircleTime : Int
                     , seed : Random.Seed 
-                    , singleCircle : List Drop
+                    , singleCircle : Int
                     , mPositionX : Int
                     , mPositionY : Int 
-                    , arrayPosition : Int}
+                    , arrayPosition : Int 
+                    , points : Int
+                    , pointsColor : List PointsC 
+                    , isPlaying : Bool }
 
 initialModel = { droplist = []
                , time = 0
                , singleCircleTime = 0
                , seed = Random.initialSeed 0
-               , singleCircle = [] 
+               , singleCircle = 0
                , mPositionX = 0
                , mPositionY = 0 
-               , arrayPosition = 0 }
+               , arrayPosition = 0
+               , points = 0 
+               , pointsColor = [] 
+               , isPlaying = False }
 
 names1 = Array.fromList ["青", "緑", "オレンジ", "赤", "ピーンク", "紫"]
 names = Array.fromList ["blue", "green", "orange", "red", "pink", "purple"]
@@ -65,6 +72,10 @@ type alias Drop = { x : Float
                   , dropColor : Color
                   , dropColorIndex : Int }
 
+type alias PointsC = { y : Float
+                     , pColor : Color
+                     , pointIndex : Int }
+
 subscriptions model = 
   Sub.batch 
   [ Mouse.clicks MouseMsg
@@ -81,27 +92,41 @@ updateWithCommand msg model =
 update : Msg -> Model -> Model
 update msg model = 
   case msg of
-    Begin -> createCircle model
-    Tick _ -> tick model
+    Begin -> createCircle { model | isPlaying = True }
+    Tick _ -> onlyUpdateIf tick model
     InitialSeed val -> { model | seed = Random.initialSeed val }
     MouseMsg position ->  checkPosition { model | mPositionX = (position.x - 350)
                                         , mPositionY = ( (-1 * position.y) + 300 )  }
 
+onlyUpdateIf tick model = 
+    if model.isPlaying then
+        tick model
+    else
+        model
+
 checkPosition model =
-    { model | droplist = List.filter (deletClickedDrops model) model.droplist }
+    updatePointsCounter { model | droplist = List.filter (deletClickedDrops model) model.droplist
+                        , points = if List.all (deletClickedDrops model) model.droplist then
+                                        model.points
+                                    else
+                                        model.points + 1 }
 
 deletClickedDrops model drop =
     not ( ( abs ((toFloat model.mPositionX) - drop.x) < 30 ) &&
-          ( abs ((toFloat model.mPositionY) - drop.y) < 30 ) )
+          ( abs ((toFloat model.mPositionY) - drop.y) < 30 ) &&
+          ( isRightColor model.singleCircle drop ) )
+
+isRightColor singleDrop clickedDrop =
+    singleDrop == clickedDrop.dropColorIndex
 
 createCircle model = 
-    creatMoreCircles 5 -200.0 model
+    createMoreCircles 5 -200.0 model
 
-creatMoreCircles numC xOffset model =
+createMoreCircles numC xOffset model =
     case numC of 
         0 -> model
         _ -> let (randomValue, newSeed) = Random.step (Random.int 0 5) model.seed in     
-                creatMoreCircles 
+                createMoreCircles 
                     (numC - 1 ) 
                     (xOffset + 100)  
                     { model | seed = newSeed
@@ -110,6 +135,7 @@ creatMoreCircles numC xOffset model =
                                         , name = getName3 randomValue
                                         , dropColor = getColor randomValue
                                         , dropColorIndex = randomValue } :: model.droplist) }
+
 getName position =
     case Array.get position names of
         Just s -> s
@@ -125,44 +151,37 @@ getPointColor i =
     case Array.get i pointColors of
         Just s -> s
         _ -> black
+
 getColor i =
     case Array.get i colors of
         Just s -> s
         _ -> red
+tick : Model -> Model
 tick model =
     model
         |> updateSingleCircle
         |> updateDrops
         |> updateTime
         |> deleteDrops
+        |> deleteExtraPoints
+        |> checkIfWon
 
 updateSingleCircle model =
     case model.singleCircleTime of
-        800 -> { model | singleCircleTime =  0 } 
+        200 -> { model | singleCircleTime =  0 } 
         1 -> createSingleCircleHelper { model | singleCircleTime = model.singleCircleTime + 1 }
         _ -> { model | singleCircleTime = model.singleCircleTime + 1 }
 
 createSingleCircleHelper model =
-    createSingleCircle 1 model
-
-createSingleCircle num model =
-    case num of 
-        0 -> model
-        _ -> let (randomValue, newSeed) = Random.step (Random.int 0 5) model.seed in     
-                createSingleCircle 
-                    (num - 1 )  
-                    { model | seed = newSeed
-                        , singleCircle = ({ x = 305.0
-                                        , y = 270.0
-                                        , name = getName2 randomValue
-                                        , dropColor = black
-                                        , dropColorIndex = randomValue } :: model.singleCircle) }
+    let (randomValue, newSeed) = Random.step (Random.int 0 (Array.length names1 - 1)) model.seed in 
+        { model | seed = newSeed
+                , singleCircle = randomValue }
 
 updateDrops model =
     { model | droplist = List.map fall model.droplist }
 
 fall drop = 
-  { drop | y = drop.y - 0.4 }
+  { drop | y = drop.y - 0.7 }
 
 deleteDrops model =
     { model | droplist = List.filter deleteDropsOffPage model.droplist}
@@ -170,21 +189,47 @@ deleteDrops model =
 deleteDropsOffPage drop =
     drop.y > -350
 
+deleteExtraPoints model =
+    { model | pointsColor = List.take model.points model.pointsColor }
+
+checkIfWon model = 
+    if model.points == 53 then
+        initialModel
+    else
+        model
+        
 updateTime model =
     case model.time of
-        300 -> addCircles { model | time = 0 }
+        200 -> addCircles { model | time = 0 }
         _ -> { model | time = model.time + 1 }
 
 addCircles model =
-    creatMoreCircles 5 -200.0 model
+    createMoreCircles 5 -200.0 model
+
+updatePointsCounter : Model -> Model
+updatePointsCounter model =
+    addUpPoints model.points -295.0 model
+
+addUpPoints : Int -> Float -> Model -> Model
+addUpPoints pointsCount yOffset model =
+    case pointsCount of
+        0 -> model
+        _ -> addUpPoints
+                (pointsCount - 1)
+                (yOffset + 10)
+                { model | pointsColor = ({ y = yOffset
+                                         , pColor = ( getPointColor model.arrayPosition)
+                                         , pointIndex = model.points } 
+                                         :: model.pointsColor)
+                        , arrayPosition = (model.arrayPosition + 1) % Array.length pointColors }
 
 drawCanvas : Model -> Html Msg
 drawCanvas model =
   List.concat [ [ backDropBox blue
                 , drawPointsBox
-                , drawSinglePointHelper model ] 
+                , drawRect model ] 
                 , (List.map drawCircle model.droplist)
-                , (List.map drawRect (List.reverse model.singleCircle) ) ]
+                , (List.map drawSinglePoint model.pointsColor) ]
     |> collage 700 600
     |> toHtml
 
@@ -193,13 +238,10 @@ drawPointsBox =
                 |> filled (hsl (degrees 256) 0.61 0.5) ]
                 |> move (310, -35)
 
-drawSinglePointHelper model =
-    drawSinglePoint (getPointColor model.arrayPosition) { model | arrayPosition = (model.arrayPosition + 1)% Array.length pointColors }
-
-drawSinglePoint color model =
+drawSinglePoint pointColor =
     group [ rect 30 10
-                |> filled color ]
-                |> move (310, -295)
+                |> filled pointColor.pColor ]
+                |> move (310, pointColor.y )
 
 drawCircle model =
     group [ circle 30
@@ -211,21 +253,25 @@ drawCircle model =
 
 drawRect model =
     group [ rect 85 60
-            |> filled model.dropColor
-          , model.name
+            |> filled black
+          , getName2 model.singleCircle
             |> fromString
             |> Text.color white
             |> Text.height 20
             |> Collage.text]
-            |> move (model.x, model.y)
+            |> move (305.0, 270.0)
 
 backDropBox color =
     group [ square 600
             |> filled color]
 
+--winBox =
+  --  group [ rect 300 100 ]
+
 view : Model -> Html Msg
 view model =
   div []
     [ drawCanvas model
-    , button [ onClick Begin ] [ Html.text "Begin" ] ]
-
+    , button [ onClick Begin ] [ Html.text "Begin" ] 
+    , div [] [ Html.text " Rules: Click on the circle that matches the text in the upper right corner." ]
+    ]
